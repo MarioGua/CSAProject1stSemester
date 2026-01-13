ed90ad69b618e4a9948d20be37f764e7080b8720
BT_1N3_34 <sample@bths.edu>
2026-01-13 10:30:52 -0500

    Second Commit

--- src/ProgramRunner.java
+++ src/ProgramRunner.java
@@ -4,10 +4,11 @@ public class ProgramRunner {
     public static void main(String[] args) {
         System.out.println("Main Logic Runs Here");
         UIHandler ui = new UIHandler();
+        Object UiFrame = ui.getFrame();
         ui.getFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ui.showFrame("Hello, this is a project for csa", "Project");
         String Name  = ui.showInputFrame("What's you're name?", "Project");
         System.out.println(Name);
-        ui.getFrame();
+
     }
 }
