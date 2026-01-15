ba937a289fa54f021f781f4298373f82defcab91
Fernando <fernandoguam31@gmail.com>
2026-01-15 18:09:50 -0500

    Home Commit

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -2,6 +2,6 @@ public class GameRunner {
     public static void main(String[] args) {
         Player player = new Player();
         System.out.println(player.getCurrentHealth());
-
+        System.out.println(player.getName());
     }
 }
